a073706f16fcd9d0fb17ba828ba13da8e480c34d
Samarth Sangam <samarthssangam@gmail.com>
2020-04-05 15:28:58 +0530

    Add Country to geocode

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 const geocode = (address, callback) => {
     
     const token = 'pk.eyJ1Ijoic2FtYXJ0aC1zYW5nYW0iLCJhIjoiY2s4Z3Y1NTQ4MDFmajNncGV6b3Y5OGlsdCJ9.8v4Tqgp__6-9vzcLgyONWA'
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?bbox=-77.083056,38.908611,-76.997778,38.959167&access_token='+ token
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) +'.json?access_token='+ token
 
     request({url, json:true }, (error, {body}) => {
         if(error) {
@@ -14,7 +14,8 @@ const geocode = (address, callback) => {
             callback(undefined, {
                 latitude: body.features[0].center[1],
                 longitude: body.features[0].center[0],
-                location: body.features[0].place_name
+                location: body.features[0].place_name,
+                Country: body.features[0].context.slice(-1)[0].text
             })
         }
     })
